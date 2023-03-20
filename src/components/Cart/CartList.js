662fd88f3f01b4a2d075098f2c6c73e3bdfbe4d4
vinit-churi <vinitchuri@hotmail.com>
2023-03-20 16:00:59 +0530

    test 01

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import CartItem from "./CartItem";
 export default class CartList extends Component {
-  render() {
-   
-    return (
-      <div></div>
-    );
-  }
+    render() {
+        const { value } = this.props;
+        const { cart } = this.props.value;
+        return (
+            <div className="container-fluid">
+                {cart.map((item) => (
+                    <CartItem key={item.id} item={item} value={value} />
+                ))}
+            </div>
+        );
+    }
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -5,16 +5,35 @@ import { storeProducts } from "../data";
 import styled from "styled-components";
 import { ProductConsumer } from "../context";
 export default class ProductList extends Component {
-  state = {
-    products: storeProducts
-  };
-  render() {
-    return (
-      <React.Fragment>
-        START HACKING
-      </React.Fragment>
-    );
-  }
+    state = {
+        products: storeProducts,
+    };
+    // console.log(this.state.products);
+    render() {
+        return (
+            <React.Fragment>
+                <ProductWrapper className="py-5">
+                    <div className="container">
+                        <Title name="our" title="products" />
+                        <div className="row">
+                            <ProductConsumer>
+                                {(value) => {
+                                    return value.products.map((product) => {
+                                        return (
+                                            <Product
+                                                key={product.id}
+                                                product={product}
+                                            />
+                                        );
+                                    });
+                                }}
+                            </ProductConsumer>
+                        </div>
+                    </div>
+                </ProductWrapper>
+            </React.Fragment>
+        );
+    }
 }
 
 const ProductWrapper = styled.section``;
