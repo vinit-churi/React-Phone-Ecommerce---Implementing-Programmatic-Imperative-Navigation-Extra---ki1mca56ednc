dcd9c9108d58f76b7b0a3791fdf71d29546553c3
VINIT CHURI <vinitchuri@hotmail.com>
2023-04-21 22:54:33 +0530

    Add files via upload

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,30 +6,27 @@ import CartTotals from "./CartTotals";
 import { ProductConsumer } from "../../context";
 import EmptyCart from "./EmptyCart";
 export default class Store extends Component {
-    render() {
-        return (
-            <section>
-                <ProductConsumer>
-                    {(value) => {
-                        const { cart } = value;
-                        if (cart.length > 0) {
-                            return (
-                                <React.Fragment>
-                                    <Title name="your" title="cart" />
-                                    <CartColumns />
-                                    <CartList value={value} />
-                                    <CartTotals
-                                        value={value}
-                                        history={this.props.history}
-                                    />
-                                </React.Fragment>
-                            );
-                        } else {
-                            return <EmptyCart />;
-                        }
-                    }}
-                </ProductConsumer>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section>
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name="your" title="cart" />
+                  <CartColumns />
+                  <CartList value={value} />
+                  <CartTotals value={value} history={this.props.history} />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
+      </section>
+    );
+  }
 }
